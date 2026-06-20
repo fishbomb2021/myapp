20758639ac89b620b4f1641970911ed256c5018e
fishbomb2021 <emily.lai.sg@gmail.com>
2026-06-21 00:15:06 +0800

    Fix Express catch-all route by using middleware for 404 fallback

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-// Define a route for GET requests to the root URL
+// Respond to GET request on the root route
 app.get('/', (req, res) => {
-  res.send('Hello World from Express!');
+  res.send('GET request to the homepage');
+});
+
+// Respond to POST request on the root route
+app.post('/', (req, res) => {
+  res.send('POST request to the homepage');
+});
+
+// Respond to GET request on the /about route
+app.get('/about', (req, res) => {
+  res.send('About page');
+});
+
+// Catch all other routes
+app.use((req, res) => {
+  res.status(404).send('404 - Page not found');
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
